38966885153c7b16a3a0566f174cfd71809ed5d6
ttmassa <102767736+ttmassa@users.noreply.github.com>
2022-07-26 12:46:10 +0200

    Update Armory.js

--- public/js/Armory.js
+++ public/js/Armory.js
@@ -1,68 +1,19 @@
-Armory = function (game, player) {
-    this.weapons = [
+Armory = function(game, player) {
+    this.weapons=[
         {
-            'name': 'Crook',
-            'model': {
+            'name':'Crook',
+            'model' : {
                 // 'meshUrl': '',
                 'meshName': 'Crook'
             },
-            'type': 'closeCombat',
-            'timeAnimation': 400,
-            'setup': {
+            'type':'closeCombat',
+            'timeAnimation' : 400,
+            'setup':{
                 // Distance de frappe de l'arme de CaC
-                'range': 2,
-                'damage': 20,
-                'cadency': 500,
-                'colorMesh': new BABYLON.Color3((59 / 255), (195 / 255), (203 / 255))
-            }
-        },
-        {
-            'name': 'Timmy',
-            'model': {
-                // 'meshUrl': '',
-                'meshName': 'Timmy'
-            },
-            'type': 'ranged',
-            'timeAnimation': 35,
-            'setup': {
-                'damage': 2,
-                'cadency': 50,
-                'ammos': {
-                    'type': 'bullet',
-                    'baseAmmos': 200,
-                    'maximum': 400,
-                    'refuel': 50
-                },
-                'colorMesh': new BABYLON.Color3((27 / 255), (235 / 255), (37 / 255))
-            }
-        },
-        {
-            'name': 'Ezekiel',
-            'model': {
-                // 'meshUrl': '',
-                'meshName': 'Ezekiel'
-            },
-            'type': 'ranged',
-            'timeAnimation': 400,
-            'setup': {
-                'damage': 30,
-                'cadency': 500,
-                'ammos': {
-                    'type': 'rocket',
-                    'baseAmmos': 15,
-                    'refuel': 10,
-                    'maximum': 40,
-                    // 'meshAmmosUrl' : '',
-                    'meshAmmosName': 'Rockets',
-                    // Rapidité de déplacement de la roquette
-                    'rocketSpeed': 6,
-                    // Taille de la roquette
-                    'rocketSize': 1.5,
-                    // Rayon de l'explosion
-                    'explosionRadius': 400
-                },
-                // Couleur du mesh par défault
-                'colorMesh': new BABYLON.Color3(1, 0.6, 0.7)
+                'range': 4,
+                'damage' : 20,
+                'cadency' : 500,
+                'colorMesh' : new BABYLON.Color3((59/255), (195/255), (203/255))
             }
         },
         {
@@ -72,12 +23,13 @@ Armory = function (game, player) {
                 'meshName': 'Armageddon'
             },
             'type': 'ranged',
-            'timeAnimation': 1400,
+            'timeAnimation': 1000,
             'setup': {
-                'damage': 1000,
+                'damage': 80,
                 'cadency': 2000,
                 'ammos': {
                     'type': 'laser',
+                    'meshAmmosName': 'Energie',
                     'spread': 1,
                     'baseAmmos': 5,
                     'maximum': 15,
@@ -85,31 +37,77 @@ Armory = function (game, player) {
                 },
                 'colorMesh': new BABYLON.Color3((133 / 255), (39 / 255), (139 / 255))
             }
+        },
+        {
+            'name':'Timmy',
+            'model' : {
+                // 'meshUrl': '',
+                'meshName': 'Timmy'
+            },
+            'type':'ranged',
+            'timeAnimation' : 50,
+            'setup':{
+                'damage' : 3,
+                'cadency' : 50,
+                'ammos' : {
+                    'type' : 'bullet',
+                    'meshAmmosName' : 'Balles',
+                    'baseAmmos' : 200,
+                    'maximum' : 400,
+                    'refuel' : 50
+                },
+                'colorMesh' : new BABYLON.Color3((27/255), (1/255), (37/255))
+            }
+        },
+        {
+            'name':'Ezekiel',
+            'timeAnimation' : 400,
+            'model' : {
+                // 'meshUrl': '',
+                'meshName': 'Ezekiel'
+            },
+            'type':'ranged',
+            'setup':{
+                'damage' : 30,
+                'cadency' : 800,
+                'ammos' : {
+                    'type' : 'rocket',
+                    'baseAmmos' : 15,
+                    'refuel' : 10,
+                    'maximum' : 40,
+                    // 'meshAmmosUrl' : '',
+                    'meshAmmosName' : 'Roquettes',
+                    // Rapidité de déplacement de la roquette
+                    'rocketSpeed' : 2,
+                    // Taille de la roquette
+                    'rocketSize' : 1,
+                    // Rayon de l'explosion
+                    'explosionRadius' : 60
+                },
+                // Couleur du mesh par défault
+                'colorMesh' : new BABYLON.Color3((209/255), (7/255), (26/255))
+            }
+        },
+    ];
+    this.bonuses=[
+        {
+            'name': 'mHealth', 
+            'message' : 'Gros pack de santé',
+            'type':'health',
+            'value': 75
+        },
+        {
+            'name':'lHealth',
+            'message' : 'Petit pack de santé',
+            'type':'health',
+            'value': 20
+        },
+        {
+            'name' : 'lArmor',
+            'message' : 'Petit pack d\'armure',
+            'type':'armor',
+            'value': 20
         }
     ];
-
-    this.bonuses = [
-        {
-            'name': 'mHealth',
-            'message': 'Gros pack de santé',
-            'type': 'health',
-            'value': 75,
-        },
-
-        {
-            'name': 'lHealth',
-            'message': 'Petit pack de santé',
-            'type': 'health',
-            'value': 20,
-        },
-
-        {
-            'name': 'mArmor',
-            'message': 'Gros pack d\'armure',
-            'type': 'armor',
-            'value': 20,
-        }
-    ];
-
     return 1
 };
